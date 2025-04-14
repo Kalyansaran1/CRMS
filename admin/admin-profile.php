05eaeba70dfa5529c25deecccd5d5bc7b255b5ba
K.kalyan saran <147470785+Kalyansaran1@users.noreply.github.com>
2025-04-14 16:18:00 +0530

    Update admin-profile.php

--- admin/admin-profile.php
+++ admin/admin-profile.php
@@ -1,133 +1,178 @@
-<?php
+<?php  
 session_start();
 error_reporting(0);
 include('includes/dbconnection.php');
 if (strlen($_SESSION['crmsaid']==0)) {
   header('location:logout.php');
-  } else{
-    if(isset($_POST['submit']))
-  {
-    $adminid=$_SESSION['crmsaid'];
-    $aname=$_POST['adminname'];
-  $mobno=$_POST['contactnumber'];
-  
-     $query=mysqli_query($con, "update tbladmin set AdminName ='$aname', MobileNumber='$mobno' where ID='$adminid'");
-    if ($query) {
-    $msg="Admin profile has been updated.";
-  }
-  else
-    {
-      $msg="Something Went Wrong. Please try again.";
-    }
-  }
-  ?>
+} else {
+  // Update for message
+  $vid=$_GET['viewid'];
+  $ret=mysqli_query($con,"update tblmessage set IsRead='1' where AppID='$vid'");
+?>
 <!DOCTYPE html>
 <html lang="zxx">
 <head>
-    <title>Campus Recruitment Management System-Admin Profile</title>
-    <!-- CSS -->
-    <link rel="stylesheet" href="assets/css/app.css">
-    <style>
-        .loader {
-            position: fixed;
-            left: 0;
-            top: 0;
-            width: 100%;
-            height: 100%;
-            background-color: #F5F8FA;
-            z-index: 9998;
-            text-align: center;
-        }
-
-        .plane-container {
-            position: absolute;
-            top: 50%;
-            left: 50%;
-        }
-    </style>
+  <title>Campus Recruitment Management System-History of applied Jobs</title>
+  <!-- CSS -->
+  <link rel="stylesheet" href="assets/css/app.css">
+  <style>
+    .loader {
+      position: fixed;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+      background-color: #F5F8FA;
+      z-index: 9998;
+      text-align: center;
+    }
+    .plane-container {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+    }
+  </style>
 </head>
 <body class="light">
 <!-- Pre loader -->
 <div id="loader" class="loader">
-    <div class="plane-container">
-        <div class="preloader-wrapper small active">
-        </div>
-    </div>
+  <div class="plane-container">
+    <div class="preloader-wrapper small active"></div>
+  </div>
 </div>
 <div id="app">
-<?php include_once('includes/sidebar.php');?>
-<!--Sidebar End-->
-<?php include_once('includes/header.php');?>
-    <div class="page has-sidebar-left">
-
-
-    <div class="animatedParent animateOnce">
-        <div class="container-fluid my-3">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="card">
-                       <header class="blue accent-3 relative">
-        <div class="container-fluid text-white">
-            <div class="row p-t-b-10 ">
-                <div class="col">
-                    <h4>
-                        <i class="icon-package"></i>
-                        Admin Profile
-                    </h4>
+<?php include_once('includes/sidebar.php'); ?>
+<?php include_once('includes/header.php'); ?>
+<div class="page has-sidebar-left">
+  <div class="animatedParent animateOnce">
+    <div class="container-fluid my-3">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card">
+            <header class="blue accent-3 relative">
+              <div class="container-fluid text-white">
+                <div class="row p-t-b-10">
+                  <div class="col">
+                    <h4><i class="icon-package"></i> Details of Applied Jobs</h4>
+                  </div>
                 </div>
-            </div>
-        </div>
-    </header>
-                        <div class="card-body b-b">
-                            <form method="post">
-                                <p style="font-size:16px; color:red" align="center"> <?php if($msg){
-    echo $msg;
-  }  ?> </p>
-  <?php
-$adminid=$_SESSION['crmsaid'];
-$ret=mysqli_query($con,"select * from tbladmin where ID='$adminid'");
-$cnt=1;
-while ($row=mysqli_fetch_array($ret)) {
-
+              </div>
+            </header>
+            <div class="card-body b-b">
+<?php
+$vid=$_GET['viewid'];
+$ret=mysqli_query($con,"SELECT tblvacancy.ID, tblvacancy.JobTitle, tblvacancy.MonthlySalary,
+  tblvacancy.JobDescriptions, tblvacancy.NoofOpenings, tblvacancy.JobLocation,
+  tblvacancy.ApplyDate, tblvacancy.LastDate, tblapplyjob.ID, tblapplyjob.Resume,
+  tblapplyjob.Message, tblapplyjob.Remark, tblapplyjob.Status, tbluser.ID AS uid,
+  tbluser.FullName, tbluser.Email, tbluser.MobileNumber, tbluser.StudentID,
+  tbluser.Gender, tbluser.Address, tbluser.Age, tbluser.DOB, tbluser.Image,
+  tblcompany.CompanyName FROM tblapplyjob 
+  JOIN tbluser ON tblapplyjob.UserId = tbluser.ID
+  JOIN tblvacancy ON tblapplyjob.JobId = tblvacancy.ID
+  JOIN tblcompany ON tblcompany.ID = tblvacancy.CompanyID 
+  WHERE tblapplyjob.ID = '$vid'");
+$cnt = 1;
+while ($row = mysqli_fetch_array($ret)) {
 ?>
-                                <div class="form-row">
-                                    <div class="form-group col-md-12">
-                                        <label for="inputEmail4" class="col-form-label">Admin Name</label>
-                                        <input type="text" class="form-control" id="adminname" name="adminname" placeholder="Admin Name" value="<?php  echo $row['AdminName'];?>">
-                                    </div>
-                                    <div class="form-group col-md-12">
-                                        <label for="inputPassword4" class="col-form-label">User Name</label>
-                                        <input type="text" id="username" name="username" class="form-control" value="<?php  echo $row['UserName'];?>" readonly="true">
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label for="inputAddress" class="col-form-label">Contact Number</label>
-                                    <input type="text" id="contactnumber" name="contactnumber" class="form-control" value="<?php  echo $row['MobileNumber'];?>">
-                                </div>
-                                <div class="form-group">
-                                    <label for="inputAddress2" class="col-form-label">Email address</label>
-                                    <input type="email" id="email" name="email" class="form-control" value="<?php  echo $row['Email'];?>" readonly='true'>
-                                </div>
-                                <div class="form-group">
-                                    <label for="inputAddress2" class="col-form-label">Registration Date</label>
-                                    <input type="text" id="regdate" name="regdate" class="form-control" value="<?php  echo $row['AdminRegdate'];?>" readonly='true'>
-                                </div>
-                               <?php } ?>
-                               
-                                <button type="submit" name="submit" class="btn btn-primary">Update</button>
-                            </form>
-                        </div>
-                
-                    </div>
-                </div>
-         
-            </div>
-        </div>
-    </div>
+<table class="table table-bordered table-hover data-tables">
+  <tr>
+    <th scope="col" width="200">Job Title</th>
+    <td><?php echo $row['JobTitle']; ?></td>
+    <th scope="col">Company Name</th>
+    <td><?php echo $row['CompanyName']; ?></td>
+  </tr>
+  <tr>
+    <th scope="col">Monthly In-hand Salary</th>
+    <td colspan="3"><?php echo $row['MonthlySalary']; ?></td>
+  </tr>
+  <tr>
+    <th scope="col">Job Descriptions</th>
+    <td colspan="3"><?php echo $row['JobDescriptions']; ?></td>
+  </tr>
+  <tr>
+    <th scope="col">Job Location</th>
+    <td><?php echo $row['JobLocation']; ?></td>
+    <th scope="col">No of Opening</th>
+    <td><?php echo $row['NoofOpenings']; ?></td>
+  </tr>
+  <tr>
+    <th scope="col">Apply Date</th>
+    <td><?php echo $row['ApplyDate']; ?></td>
+    <th scope="col">Last Date</th>
+    <td><?php echo $row['LastDate']; ?></td>
+  </tr>
+  <tr>
+    <th scope="col">Full Name</th>
+    <td><?php echo $row['FullName']; ?></td>
+    <th scope="col">Email</th>
+    <td><?php echo $row['Email']; ?></td>
+  </tr>
+  <tr>
+    <th scope="col">Mobile Number</th>
+    <td><?php echo $row['MobileNumber']; ?></td>
+    <th scope="col">Student ID</th>
+    <td><?php echo $row['StudentID']; ?></td>
+  </tr>
+  <tr>
+    <th scope="col">Gender</th>
+    <td><?php echo $row['Gender']; ?></td>
+    <th scope="col">Address</th>
+    <td><?php echo $row['Address']; ?></td>
+  </tr>
+  <tr>
+    <th scope="col">Age</th>
+    <td><?php echo $row['Age']; ?></td>
+    <th scope="col">DOB</th>
+    <td><?php echo $row['DOB']; ?></td>
+  </tr>
+  <tr>
+    <th scope="col">Image</th>
+    <td><img src="../user/images/<?php echo $row['Image']; ?>" width="200" height="150"></td>
+    <th scope="col">Education Detail</th>
+    <td><a href="view-education-detail.php?eduid=<?php echo $row['uid']; ?>&uname=<?php echo $row['FullName']; ?>" target="_blank">My Education Details</a></td>
+  </tr>
+  <tr>
+    <th scope="col">Resume</th>
+    <td><a href="../user/images/<?php echo $row['Resume']; ?>" target="_blank">Download</a></td>
+    <th scope="col">Status</th>
+    <td>
+      <?php echo $row['Status'] == "" ? "Not Responded Yet" : $row['Status']; ?>
+    </td>
+  </tr>
+</table>
+<?php if ($row['Status'] != '0') {
+$ret = mysqli_query($con, "SELECT tblmessage.Message, tblmessage.Status AS comstatus, tblmessage.ResponseDate FROM tblapplyjob LEFT JOIN tblmessage ON tblmessage.AppID = tblapplyjob.ID WHERE tblapplyjob.ID = '$vid'");
+$cnt = 1;
+?>
+<table id="datatable" class="table table-bordered dt-responsive nowrap" style="width: 100%;">
+  <tr align="center">
+    <th scope="col" colspan="4" style="font-size:18px">Application History</th>
+  </tr>
+  <tr>
+    <th scope="col">#</th>
+    <th scope="col">Message</th>
+    <th scope="col">Status</th>
+    <th scope="col">Time</th>
+  </tr>
+  <?php while ($row = mysqli_fetch_array($ret)) { ?>
+  <tr>
+    <td><?php echo $cnt++; ?></td>
+    <td><?php echo $row['Message']; ?></td>
+    <td><?php echo $row['comstatus']; ?></td>
+    <td><?php echo $row['ResponseDate']; ?></td>
+  </tr>
+  <?php } ?>
+</table>
+<?php } } ?>
+</div>
+</div>
+</div>
+</div>
+</div>
 </div>
 <div class="control-sidebar-bg shadow white fixed"></div>
 </div>
-<!--/#app -->
 <script src="assets/js/app.js"></script>
 </body>
 </html>
